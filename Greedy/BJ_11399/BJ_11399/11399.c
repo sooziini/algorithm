930200956e2724fc390da8bd9c4d7543e736d4c2
sooooz <soojinpar1026@gmail.com>
2020-11-02 16:16:36 +0900

    solve BJ_11399 (ATM)

--- Greedy/BJ_11399/BJ_11399/11399.c
+++ Greedy/BJ_11399/BJ_11399/11399.c
@@ -1,5 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+#define SWAP(x, y, t) ((t)=(x), (x)=(y), (y)=(t))
 
+void bubble_sort(int arr[], int n) {
+	int tmp;
+	for (int i = n - 1; i > 0; i--) {
+		for (int j = 0; j < i; j++)
+			if (arr[j] > arr[j + 1])
+				SWAP(arr[j], arr[j + 1], tmp);
+	}
+}
+
+int main() {
+	int n, sum = 0;
+	scanf("%d", &n);
+	int* a = malloc(sizeof(int) * n);
+	
+	for (int i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+
+	bubble_sort(a, n);		// 오름차순으로 정렬
+	
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j <= i; j++)
+			sum += a[j];
+	
+	printf("%d", sum);
 }
