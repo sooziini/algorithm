ae6e7988c73ef17bf35a5ca7c59f2449fe36fa80
sooziini <soojinpar1026@gmail.com>
2021-05-19 12:04:37 +0900

    scofe-3 (상품 배치 추천) 수정

--- scofe-3.py
+++ scofe-3.py
@@ -16,13 +16,29 @@ def chk(x, y):
 def calc(n, x, y):
     global total
     global ret
+    flag = False
     for i in range(n):
-        if (x + i >= n or y + i >= n):
+        if (x + i >= n or y + i >= n or flag):
             break
         # 현재 좌표의 오른쪽, 아래쪽, 대각선 확인
         if (chk(x+i, y) and chk(x, y+i) and chk(x+i, y+i)):
-            total += 1
-            ret[i] += 1
+            # 해당 정사각형이 3x3 이상일 경우
+            if (i > 1):
+                j = 1
+                while (j < i):
+                    if (chk(x+i-j, y+i) and chk(x+i, y+i-j)):
+                        total += 1
+                        ret[i] += 1
+                        j += 1
+                    else:
+                        flag = True
+                        break
+            # 해당 정사각형이 1x1 or 2x2일 경우
+            else:
+                total += 1
+                ret[i] += 1
+        else:
+            break
 
 for i in range(n):
     for j in range(n):
@@ -34,4 +50,4 @@ print("total: %d" %total)
 for i in range(n):
     if (ret[i] == 0):
         continue
-    print("size[%d]: %d" %(i, ret[i]))
+    print("size[%d]: %d" %(i+1, ret[i]))
