4e3b2b1e8fc8e559f112d77ae55f48e8aba279cb
sooziini <soojinpar1026@gmail.com>
2021-12-25 22:56:53 +0900

    refactoring 스킬트리

--- Programmers/[코테연습]스킬트리.py
+++ Programmers/[코테연습]스킬트리.py
@@ -1,6 +1,31 @@
-# 스킬트리
+# 스킬 트리 (Summer/Winter Coding ~2018)
 
 def solution(skill, skill_trees):
+    answer=0
+    for sk in skill_trees:  # "BACDE"
+        skill_list=list(skill)
+        for s in sk:    # "B"
+            if s in skill:  # skill에 해당되는지 확인
+                if s != skill_list.pop(0):
+                    break
+        else:
+            answer+=1
+    return answer
+
+def solution2(skill, skill_trees):
+    answer = 0
+    for sk in skill_trees:  # "BACDE"
+        cur_idx = 0     # skill index
+        for s in sk:    # "B"
+            if s in skill:      # skill에 해당되는지 확인
+                if skill[cur_idx]!=s:   # 선행스킬 안함
+                    break
+                cur_idx+=1
+        else:
+            answer+=1
+    return answer
+
+def solution3(skill, skill_trees):
     answer = 0
     for sk in skill_trees:  # "BACDE"
         cur_idx = 0     # skill index
