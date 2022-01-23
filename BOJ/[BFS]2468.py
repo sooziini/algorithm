ebfd963b89cada9b2f934e9c673532c056019cbc
sooziini <soojinpar1026@gmail.com>
2022-01-23 16:52:54 +0900

    refactoring 19236 청소년 상어 (deepcopy -> visited 사용)

--- BOJ/[BFS]2468.py
+++ BOJ/[BFS]2468.py
@@ -1,7 +1,6 @@
 # 안전 영역 - BFS
 
 import sys
-from copy import deepcopy
 from collections import deque
 
 n=int(sys.stdin.readline())
@@ -13,33 +12,34 @@ def get_max(area):
         maxi=max(maxi, max(a))
     return maxi
 
-def change_area(area, num):
+def set_visited(area, num):
+    visited=[[True]*n for _ in range(n)]
     for i in range(n):
         for j in range(n):
             if area[i][j]<=num:
-                area[i][j]=0
-    return area
+                visited[i][j]=False
+    return visited
 
 dx,dy=[-1, 1, 0, 0],[0, 0, -1, 1]
-def bfs(i, j, area):
+def bfs(i, j, visited):
     q=deque([(i, j)])
-    area[i][j]=0
+    visited[i][j]=False
     while q:
         x,y=q.popleft()
         for d in range(4):
             nx,ny=x+dx[d],y+dy[d]
-            if 0<=nx<n and 0<=ny<n and area[nx][ny]!=0:
-                area[nx][ny]=0
+            if 0<=nx<n and 0<=ny<n and visited[nx][ny]:
+                visited[nx][ny]=False
                 q.append((nx, ny))
 
 answer=[]
 for m in range(0, get_max(area)+1):
-    area_copy=change_area(deepcopy(area), m)
+    visited=set_visited(area, m)
     cnt=0
     for i in range(n):
         for j in range(n):
-            if area_copy[i][j]!=0:
-                bfs(i, j, area_copy)
+            if visited[i][j]:
+                bfs(i, j, visited)
                 cnt+=1
     answer.append(cnt)
 
