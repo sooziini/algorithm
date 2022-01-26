10daa7f2b7b9cc972b360e06d8e3d6c06dc86e24
sooziini <soojinpar1026@gmail.com>
2022-01-27 00:52:33 +0900

    BOJ 1463 1로 만들기 (DP)

---
+++ BOJ/[DP]1463.py
@@ -0,0 +1,18 @@
+# 1로 만들기 - DP
+
+import sys
+
+n=int(sys.stdin.readline())
+
+dp=[0]*(n+1)
+for i in range(2, n+1):
+    if i%6==0:
+        dp[i]=min(dp[i//3], dp[i//2], dp[i-1])+1
+    elif i%3==0:
+        dp[i]=min(dp[i//3], dp[i-1])+1
+    elif i%2==0:
+        dp[i]=min(dp[i//2], dp[i-1])+1
+    else:
+        dp[i]=dp[i-1]+1
+
+print(dp[n])
