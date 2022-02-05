f313cb7e52ed1cc91c359eb506f9b8c55574291e
sooziini <soojinpar1026@gmail.com>
2022-02-05 23:24:39 +0900

    refactoring 14226 이모티콘 (가독성을 위해 not 사용)

--- BOJ/[BFS]14226.py
+++ BOJ/[BFS]14226.py
@@ -10,13 +10,13 @@ def bfs():
     visited=[[0]*(s+1) for _ in range(s+1)]
     while q:
         e,c=q.popleft()
-        if visited[e][e]==0:
+        if not visited[e][e]:
             visited[e][e]=visited[e][c]+1
             q.append((e, e))
-        if e+c<=s and visited[e+c][c]==0:
+        if e+c<=s and not visited[e+c][c]:
             visited[e+c][c]=visited[e][c]+1
             q.append((e+c, c))
-        if e-1>0 and visited[e-1][c]==0:
+        if e-1>0 and not visited[e-1][c]:
             visited[e-1][c]=visited[e][c]+1
             q.append((e-1, c))
     return min([v for v in visited[s] if v!=0])
