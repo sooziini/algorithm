dd9147bdf00f5d8d034c24d13ea685f618e1ee79
sooziini <soojinpar1026@gmail.com>
2022-04-08 22:45:40 +0900

    BOJ 2933 미네랄 (BFS)

--- BOJ/[BFS]2933.py
+++ BOJ/[BFS]2933.py
@@ -23,28 +23,30 @@ def bfs(i, j):
     while q:
         x, y = q.popleft()
         if x == R-1:
-            return
+            return False
         fall_list.append((x, y))
         for d in range(4):
             nx, ny = x + dx[d], y + dy[d]
             if 0 <= nx < R and 0 <= ny < C and cave[nx][ny] == 'x' and not visited[nx][ny]:
                 visited[nx][ny] = True
                 q.append((nx, ny))
-    fall(fall_list)
+    fall(fall_list, visited)
+    return True
 
-def fall(fall_list):
+def fall(fall_list, visited):
     fall_cnt = int(1e9)
     for x, y in fall_list:
         cnt = 0
-        while x+1 < R and cave[x+1][y] == '.':
+        for i in range(x+1, R):
+            if cave[i][y]=='x':
+                break
             cnt += 1
-            x += 1
-        if (x+1, y) not in fall_list:
+        if not visited[i][y]:
             fall_cnt = min(fall_cnt, cnt)
-
-    for x, y in fall_list:
-        cave[x][y]='.'
-        cave[x+fall_cnt][y]='x'
+    if 0 < fall_cnt < int(1e9):
+        for x, y in sorted(fall_list, reverse=True):
+            cave[x][y] = '.'
+            cave[x+fall_cnt][y] = 'x'
 
 for idx, t in enumerate(turn):
     cur = cave[R-t]
@@ -55,8 +57,9 @@ for idx, t in enumerate(turn):
             for d in range(4):
                 nx, ny = x + dx[d], y + dy[d]
                 if 0 <= nx < R and 0 <= ny < C and cave[nx][ny] == 'x':
-                    bfs(nx, ny)
+                    if bfs(nx, ny):
+                        break
             break
 
-for c in cave:
-    print(''.join(c))
+for idx, c in enumerate(cave):
+    print(''.join(c), end='\n' if idx+1 < len(cave) else '')
